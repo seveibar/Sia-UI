5fc93445f4db50f35c9aa9b22a5e702b772d9f86
Severin Ibarluzea <seve@genesee-60.dynamic2.rpi.edu>
2015-03-12 23:51:54 -0400

    support for mac os

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,12 +21,6 @@ module.exports = function(grunt){
               "site/stylesheets/style.css": "site/stylesheets/main.less" // destination file and source file
             }
           }
-        },
-
-        shell: {
-            target: {
-                command: path.join('atom-shell','atom') + ' .'
-            }
         }
 
     });
@@ -39,6 +33,21 @@ module.exports = function(grunt){
     grunt.registerTask('default', ['build', 'download-atom-shell']);
     grunt.registerTask('build', ['execute', 'less']);
     grunt.registerTask('test', ['build']);
-    grunt.registerTask('run', ['default', 'shell']);
+    //grunt.registerTask('run', ['default', 'shell']);
+    grunt.registerTask('run', "Running Sia UI with atom shell...", function(){
+        grunt.task.run("default");
+        var cp = require("child_process");
+        if (process.platform == "darwin"){
+            cp.execSync("./atom-shell/Atom.app/Contents/MacOS/Atom .", function(err,stdout,stderr){
+                console.error(err,stderr);
+                console.log(stdout);
+           });
+        }else{
+            cp.execSync(path.join("atom-shell","atom") + " .", function(err,stdout,stderr){
+                console.error(err,stderr);
+                console.log(stdout);
+            });
+        }
+    });
 
 };
